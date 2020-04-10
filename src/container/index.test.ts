77d97c8e84e450f68ecc177f57795fd39d81f484
Issa Shabo <issashabo@gmail.com>
2020-04-10 15:58:56 +0100

    test: test for substitution

--- src/container/index.test.ts
+++ src/container/index.test.ts
@@ -1,5 +1,6 @@
 import { Container } from '.';
 import { Injectable } from '../decorators';
+
 /* tslint:disable: max-classes-per-file */
 describe('Container', () => {
   console.log = jest.fn();
@@ -10,17 +11,39 @@ describe('Container', () => {
     }
   }
 
-  @Injectable()
-  class SomeService {
-    constructor(private readonly logger: Logger) {
-      this.logger.log('SomeService intinialized');
+  class MyLogger {
+    public log(...args: any[]) {
+      console.log('[my-log]', ...args);
     }
   }
 
+  @Injectable()
+  class SomeService {
+    constructor(private readonly logger: Logger) {
+      this.logger.log('SomeService initialized');
+    }
+  }
+
+  afterEach(() => {
+    (console.log as any).mockClear();
+  });
   it('injects Logger into SomeService', () => {
     const container = new Container();
     container.register({ ref: Logger, source: Logger, type: 'class' });
     const someService = container.construct(SomeService);
-    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(
+      '[log]',
+      'SomeService initialized'
+    );
+  });
+
+  it('can substitute for another dependency of the same type', () => {
+    const container = new Container();
+    container.register({ ref: Logger, source: MyLogger, type: 'class' });
+    const someService = container.construct(SomeService);
+    expect(console.log).toHaveBeenCalledWith(
+      '[my-log]',
+      'SomeService initialized'
+    );
   });
 });
